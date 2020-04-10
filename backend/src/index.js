7eddc259040f1995d4d830abbfc474b38e165876
Lucas Amaral <lucasamaral@MacBook-Pro-de-Lucas.local>
2020-04-10 08:04:29 -0300

    migrations

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-const routes = express.Router();
-
+const routes = require('./routes')
+app.use(express.json());
 app.use(routes);
 
-routes.get('/', (req, res) => {
-    res.send('Server on')
-})
-app.listen(port, ()=>{
+
+app.listen(port, () => {
     console.log('Server on at port', port);
 })
