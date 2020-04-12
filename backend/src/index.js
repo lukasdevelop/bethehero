46aa324f307dd354aee8a28f9fbd1e882f3f7098
Lucas Amaral <lucasamaral@MacBook-Pro-de-Lucas.local>
2020-04-12 09:41:00 -0300

    fim do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const port = 3000;
 const routes = require('./routes')
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const routes = express.Router();
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
-routes.get('/', (req, res) => {
-    res.send('Server on')
-})  
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-module.exports = routes;
+routes.post('/session', SessionController.index);
+
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+module.exports = routes;    
